da83d267d8610e2238359623b99a3e1247ae8a7f
烈e朵朵 <18809810730@139.com>
2020-12-18 17:29:52 +0800

    ConvertUtils

--- widgets/src/main/java/com/helper/widgets/banner/adapter/BannerPageAdapterHelper.java
+++ widgets/src/main/java/com/helper/widgets/banner/adapter/BannerPageAdapterHelper.java
@@ -1,11 +1,10 @@
 package com.helper.widgets.banner.adapter;
 
+import android.content.res.Resources;
 import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.helper.utils.ConvertUtils;
 
 public class BannerPageAdapterHelper {
     public static int sPagePadding = 0;
@@ -13,15 +12,15 @@ public class BannerPageAdapterHelper {
 
     public void onCreateViewHolder(ViewGroup parent, View itemView) {
         RecyclerView.LayoutParams lp = (RecyclerView.LayoutParams) itemView.getLayoutParams();
-        lp.width = parent.getWidth() - ConvertUtils.dp2px(2 * (sPagePadding + sShowLeftCardWidth));
+        lp.width = parent.getWidth() - dp2px(2 * (sPagePadding + sShowLeftCardWidth));
         itemView.setLayoutParams(lp);
     }
 
     public void onBindViewHolder(View itemView, final int position, int itemCount) {
-        int padding = ConvertUtils.dp2px(sPagePadding);
+        int padding = dp2px(sPagePadding);
         itemView.setPadding(padding, 0, padding, 0);
-        int leftMarin = position == 0 ? padding + ConvertUtils.dp2px(sShowLeftCardWidth) : 0;
-        int rightMarin = position == itemCount - 1 ? padding + ConvertUtils.dp2px(sShowLeftCardWidth) : 0;
+        int leftMarin = position == 0 ? padding + dp2px(sShowLeftCardWidth) : 0;
+        int rightMarin = position == itemCount - 1 ? padding + dp2px(sShowLeftCardWidth) : 0;
         setViewMargin(itemView, leftMarin, 0, rightMarin, 0);
     }
 
@@ -39,5 +38,10 @@ public class BannerPageAdapterHelper {
 
     public void setShowLeftCardWidth(int showLeftCardWidth) {
         sShowLeftCardWidth = showLeftCardWidth;
+    }
+
+    private int dp2px(final float dpValue) {
+        final float scale = Resources.getSystem().getDisplayMetrics().density;
+        return (int) (dpValue * scale + 0.5f);
     }
 }
